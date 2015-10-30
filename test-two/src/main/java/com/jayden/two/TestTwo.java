4b0be3f893073f94ab692a7aef434042b4bb1c01
unwillingman <at3134165@126.com>
2015-10-30 18:40:20 +0800

    test module

---
+++ test-two/src/main/java/com/jayden/two/TestTwo.java
@@ -0,0 +1,20 @@
+package com.jayden.two;
+
+/**
+ * √Ë ˆ£∫
+ * <pre>
+ * HISTORY
+ * ****************************************************************************
+ *  ID   DATE            PERSON          REASON
+ *  1    2015-10-30       089245          Create
+ * ****************************************************************************
+ * </pre>
+ *
+ * @author 089245
+ * @since 1.0
+ */
+public class TestTwo {
+    public static void main(String[] args) {
+        System.out.println("test two");
+    }
+}
---
+++ test-war/src/main/java/com/jayden/war/TestWar.java
@@ -0,0 +1,24 @@
+package com.jayden.war;
+
+import com.jayden.two.TestTwo;
+
+/**
+ * √Ë ˆ£∫
+ * <pre>
+ * HISTORY
+ * ****************************************************************************
+ *  ID   DATE            PERSON          REASON
+ *  1    2015-10-30       089245          Create
+ * ****************************************************************************
+ * </pre>
+ *
+ * @author 089245
+ * @since 1.0
+ */
+public class TestWar {
+    public static void main(String[] args) {
+        System.out.println("test war");
+        TestTwo.main(null);
+
+    }
+}
